142fa242f55592c3b8d779b27fc5c7727177d56b
albertrein <guilhermealberto8@gmail.com>
2020-04-16 23:52:25 -0300

    Saving KMZ file

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const service = require('./service');
 app.listen(3000);
 
 app.get('/', (request,response) => {
-   service();
+   service(request.query.mid);
     response.send('<h1>Hello World</h1>')
 });
--- service.js
+++ service.js
@@ -1,8 +1,25 @@
 const JsZip = require('jszip');
-const geoJson = require('geojson');
-const file = require('fs');
+//const geoJson = require('geojson');
+const fs = require('fs');
+const http = require('https');
 
-module.exports = () => {
-    
-        
+module.exports = async (mid) => {
+    let timestamp = Date.now();
+    let exit = await saveFile(mid, timestamp);
+    console.log(exit);
 }
+
+getFileKMZ = (mid,timestamp) => new Promise((resolve,reject) => {
+    const file = fs.createWriteStream(timestamp+".kmz");
+    http.get("https://www.google.com/maps/d/kml?mid="+mid, function(response) {
+      response.pipe(file);
+      resolve(true);
+    });
+});
+
+
+deleteTempFile = (timestamp) => new Promise((resolve,reject) => {
+    fs.unlink(timestamp+".kmz",()=>{
+        resolve();
+    });
+});
