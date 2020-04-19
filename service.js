3374e1fec84596154df1655f4136b0f98b012cd1
albertrein <guilhermealberto8@gmail.com>
2020-04-19 19:32:00 -0300

    Testing saves

--- service.js
+++ service.js
@@ -1,22 +1,46 @@
 const JsZip = require('jszip');
-//const geoJson = require('geojson');
+const domparser = require('dom-parser');
+const dom = new domparser();
 const fs = require('fs');
 const http = require('https');
+const fetch = require('fetch');
 
 module.exports = async (mid) => {
     let timestamp = Date.now();
-    let exit = await saveFile(mid, timestamp);
-    console.log(exit);
+    let exit = await saveFileKMZ(mid, timestamp);
+    //readFileKMZ(timestamp);
 }
 
-getFileKMZ = (mid,timestamp) => new Promise((resolve,reject) => {
-    const file = fs.createWriteStream(timestamp+".kmz");
-    http.get("https://www.google.com/maps/d/kml?mid="+mid, function(response) {
-      response.pipe(file);
-      resolve(true);
+saveFileKMZ = (mid,timestamp) => new Promise((resolve,reject) => {
+    fetch.FetchStream('https://www.google.com/maps/d/kml?mid='+mid).then(res => res.buffer).then(buffer => {console.log('asd')})
+});
+
+readFileKMZ = (timestamp) => new Promise(async (resolve,reject) => {
+    
+    
+    fs.readFile(timestamp+".kmz", function(err, data) {
+        if (err) throw err;
+        JsZip.loadAsync(data).then(function (zip) {
+            console.log('massa')
+        });
     });
 });
 
+let getDom = xml => (new DOMParser()).parseFromString(xml, "text/xml")
+let getExtension = fileName => fileName.split(".").pop()
+
+getKmlDom = async (fileReaded) => {
+    let jszip = new JsZip();
+    return jszip.get  .loadAsync(fileReaded).then(zip => {
+        zip.forEach((relPath, fileReaded) => {
+            if (getExtension(relPath) === "kml" && kmlDom === null) {
+                kmlDom = fileReaded.async("string").then(getDom)
+            }
+        })
+        return kmlDom || Promise.reject("No kml file found") 
+    });
+    
+}
 
 deleteTempFile = (timestamp) => new Promise((resolve,reject) => {
     fs.unlink(timestamp+".kmz",()=>{
