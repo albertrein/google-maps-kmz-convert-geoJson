479529c01a5d4b2b788531d609ec6770cbdc950c
albertrein <guilhermealberto8@gmail.com>
2020-04-21 09:42:15 -0300

    Making verification of file.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const service = require('./service');
 app.listen(process.env.PORT || 3000);
 
 app.get('/', async (request,response) => {
+    let mid = request.query.mid;
+    if(!mid || mid == ""|| mid == " "){
+        return ({"Invalid Map Id" : "Use: GET mid=googleMapId"})
+    }
    let jsonMap = await service.load(request.query.mid);
     response.send(jsonMap);
 });
