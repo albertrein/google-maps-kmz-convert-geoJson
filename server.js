2d20230f8112e5447b7373b81d09a3546785515d
albertrein <guilhermealberto8@gmail.com>
2020-04-21 08:59:05 -0300

    Server listen port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const service = require('./service');
-app.listen(3000);
+
+app.listen(process.env.PORT || 3000);
 
 app.get('/', async (request,response) => {
    let jsonMap = await service.load(request.query.mid);
