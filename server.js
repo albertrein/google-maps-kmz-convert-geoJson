46a40f4ca13654feb02c82352d9470a9c5ac993e
albertrein <guilhermealberto8@gmail.com>
2020-04-20 23:51:46 -0300

    Config server reponde

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const service = require('./service');
 app.listen(3000);
 
-app.get('/', (request,response) => {
-   service(request.query.mid);
-    response.send('<h1>Hello World</h1>')
+app.get('/', async (request,response) => {
+   let jsonMap = await service.load(request.query.mid);
+    response.send(jsonMap);
 });
