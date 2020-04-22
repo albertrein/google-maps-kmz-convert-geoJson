309ecabf59ad0de64d460cd1b9714af5734313dd
albertrein <guilhermealberto8@gmail.com>
2020-04-21 22:57:46 -0300

    Fixing issue response error.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen(process.env.PORT || 3000);
 app.get('/', async (request,response) => {
     let mid = request.query.mid;
     if(!mid || mid == ""|| mid == " "){
-        return ({"Invalid Map Id" : "Use: GET mid=googleMapId"})
+        response.send({"Invalid Map Id" : "Use: GET mid=googleMapId"})
     }
    let jsonMap = await service.load(request.query.mid);
     response.send(jsonMap);
